9d966f269a7dfbf002f82c00b9a3fba5157337a8
Oliver Smith-Denny <osde@microsoft.com>
2026-09-08 21:07:02 -0700

    GCD: Fix is_efi_memory_map_descriptor Length Check
    is_efi_memory_map_descriptor was checking for sub-page
    size lengths by seeing if (length + 0xFFF) / 0x1000 == 0.
    However, this can only return 0 if length is 0.
    
    This commit fixes this check and broadens it to reject any
    non-page aligned length as required by the GCD and EFI_MEMORY_MAP.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- patina_dxe_core/src/gcd/spin_locked_gcd.rs
+++ patina_dxe_core/src/gcd/spin_locked_gcd.rs
@@ -1242,10 +1242,9 @@ impl GCD {
     /// * `None` if the descriptor should be excluded
     fn is_efi_memory_map_descriptor(descriptor: &MemorySpaceDescriptor) -> Option<efi::MemoryType> {
         // Validate page alignment and size
-        let number_of_pages = ((descriptor.length as usize + UEFI_PAGE_MASK) / UEFI_PAGE_SIZE) as u64;
-        if number_of_pages == 0 {
-            debug_assert!(false, "GCD returned a memory descriptor smaller than a page.");
-            return None; // skip entries for things smaller than a page
+        if !descriptor.length.is_multiple_of(UEFI_PAGE_SIZE as u64) || descriptor.length == 0 {
+            debug_assert!(false, "GCD returned a non-page aligned memory descriptor.");
+            return None; // skip entries for non-page aligned entries
         }
         if !descriptor.base_address.is_multiple_of(UEFI_PAGE_SIZE as u64) {
             debug_assert!(false, "GCD returned a non-page-aligned memory descriptor.");
@@ -6809,6 +6808,36 @@ mod tests {
             };
 
             assert_eq!(GCD::is_efi_memory_map_descriptor(&descriptor), Some(efi::CONVENTIONAL_MEMORY));
+        });
+    }
+
+    #[test]
+    fn test_is_efi_memory_map_descriptor_unaligned() {
+        with_locked_state(|| {
+            // Unaligned descriptors are dropped
+            let descriptor = dxe_services::MemorySpaceDescriptor {
+                memory_type: GcdMemoryType::SystemMemory,
+                base_address: 0x2001,
+                length: UEFI_PAGE_SIZE as u64,
+                capabilities: efi::MEMORY_WB,
+                attributes: efi::MEMORY_WB,
+                image_handle: core::ptr::null_mut(),
+                device_handle: core::ptr::null_mut(),
+            };
+
+            assert_eq!(GCD::is_efi_memory_map_descriptor(&descriptor), None);
+
+            let descriptor = dxe_services::MemorySpaceDescriptor {
+                memory_type: GcdMemoryType::SystemMemory,
+                base_address: 0x2000,
+                length: (UEFI_PAGE_SIZE - 1) as u64,
+                capabilities: efi::MEMORY_WB,
+                attributes: efi::MEMORY_WB,
+                image_handle: core::ptr::null_mut(),
+                device_handle: core::ptr::null_mut(),
+            };
+
+            assert_eq!(GCD::is_efi_memory_map_descriptor(&descriptor), None);
         });
     }
 
